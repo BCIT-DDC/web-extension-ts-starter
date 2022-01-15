fcf9412aeec3728f0e72b6f0f419ebdb03b06300
iflinda <informlinda@gmail.com>
2022-01-15 13:13:41 -0800

    Removed logic from Pop.tsx file

---
+++ README.md
@@ -0,0 +1,3 @@
+# nwPlus Hackathon Extension
+
+This web extension starter repository was created via the code base at: 
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,69 +1,11 @@
 import React from 'react';
+// import { browser, Tabs } from 'webextension-polyfill-ts';
 
-import Hello from '@components/Hello';
-import { browser, Tabs } from 'webextension-polyfill-ts';
-import Scroller from '@components/Scroller';
-
-// Scripts to execute in current tab
-const scrollToTopScript = `window.scroll(0,0)`;
-const scrollToBottomScript = `window.scroll(0,9999999)`;
-
-/**
- * Executes a string of Javascript on the current tab
- * @param code - The string of code to execute on the current tab
- */
-async function executeScript(code: string): Promise<void> {
-    // Query for the active tab in the current window
-    try {
-        const tabs: Tabs.Tab[] = await browser.tabs.query({
-            active: true,
-            currentWindow: true,
-        });
-
-        const currentTab: Tabs.Tab | undefined = tabs[0];
-
-        if (!currentTab) {
-            return;
-        }
-
-        console.log(currentTab.url);
-
-        if (currentTab.url?.includes('github')) {
-            console.log('Where on github');
-
-            await browser.tabs.executeScript(currentTab.id, {
-                code,
-            });
-
-            console.log('Done Scrolling');
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
+// Removed all existing code from the starter file.
 
 const Popup = () => {
-    // Sends the `popupMounted` event
-    React.useEffect(() => {
-        browser.runtime.sendMessage({ popupMounted: true });
-    }, []);
-
-    // Renders the component tree
-    return (
-        <div className="popupContainer">
-            <div className="mx-4 my-4">
-                <Hello />
-                <hr />
-                <Scroller
-                    onClickScrollTop={() => {
-                        executeScript(scrollToTopScript);
-                    }}
-                    onClickScrollBottom={() => {
-                        executeScript(scrollToBottomScript);
-                    }}
-                />
-            </div>
-        </div>
+    return ( 
+    <div><p>Test</p></div>
     );
 };
 
