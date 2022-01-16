fec6900ca947f4c6df450d4cb7dceff294436944
matteomiceli <omatteomiceli@gmail.com>
2022-01-15 23:46:33 -0800

    working on displaying content

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -19,12 +19,20 @@ class Popup extends Component {
         this.connectSocket();
         this.socket.on('text', content => {
             console.log(content);
+            localStorage.setItem('content', content);
         });
     }
 
-    componentDidUpdate() {
-        console.log(this.state.roomId);
-        this.joinRoom();
+    componentDidUpdate(
+        prevProps: any,
+        prevState: { roomId: string; content: string },
+    ) {
+        if (prevState.roomId !== this.state.roomId) {
+            this.leaveRoom(prevState.roomId);
+            this.joinRoom();
+        } else if (prevState.content !== this.state.content) {
+            this.displayContent();
+        }
     }
 
     connectSocket(): void {
@@ -35,6 +43,10 @@ class Popup extends Component {
 
     joinRoom(): void {
         this.socket.emit('join', this.state.roomId);
+    }
+
+    leaveRoom(roomId: string): void {
+        this.socket.emit('leave', roomId);
     }
 
     generateRoomId(): void {
@@ -48,14 +60,18 @@ class Popup extends Component {
         (document.getElementById('copyText') as HTMLInputElement).value = '';
     };
 
-    copy = () => {
+    displayContent() {
         const content = (
             document.getElementById('copyText') as HTMLInputElement
         ).value;
+        this.setState({ content });
         localStorage.setItem('content', content);
-        if (content.length !== 0) {
-            navigator.clipboard.writeText(content);
-        } else if (content.length === 0) {
+    }
+
+    copy = () => {
+        if (this.state.content.length !== 0) {
+            navigator.clipboard.writeText(this.state.content);
+        } else if (this.state.content.length === 0) {
             navigator.clipboard.writeText('');
         }
     };
@@ -116,16 +132,16 @@ class Popup extends Component {
                                     <path
                                         d="M8 4V16C8 16.5304 8.21071 17.0391 8.58579 17.4142C8.96086 17.7893 9.46957 18 10 18H18C18.5304 18 19.0391 17.7893 19.4142 17.4142C19.7893 17.0391 20 16.5304 20 16V7.242C20 6.97556 19.9467 6.71181 19.8433 6.46624C19.7399 6.22068 19.5885 5.99824 19.398 5.812L16.083 2.57C15.7094 2.20466 15.2076 2.00007 14.685 2H10C9.46957 2 8.96086 2.21071 8.58579 2.58579C8.21071 2.96086 8 3.46957 8 4V4Z"
                                         stroke="#494949"
-                                        stroke-width="2"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
                                     />
                                     <path
                                         d="M16 18V20C16 20.5304 15.7893 21.0391 15.4142 21.4142C15.0391 21.7893 14.5304 22 14 22H6C5.46957 22 4.96086 21.7893 4.58579 21.4142C4.21071 21.0391 4 20.5304 4 20V9C4 8.46957 4.21071 7.96086 4.58579 7.58579C4.96086 7.21071 5.46957 7 6 7H8"
                                         stroke="#494949"
-                                        stroke-width="2"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
                                     />
                                 </svg>
                             </button>
